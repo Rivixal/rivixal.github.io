bbd7ca4040ebb8d60756b0e2bc26a80dace2ecc2
Maydilsiel Software <93471604+Maydilsiel@users.noreply.github.com>
2023-07-05 21:54:28 +0300

    Delete script directory

--- script/downloads.js
+++
@@ -1,8 +0,0 @@
-var downloadCount = 0;
-
-function incrementDownloadCount() {
-  downloadCount++;
-  document.getElementById("downloadButton").innerHTML = "Скачать (" + downloadCount + ")";
-}
-
-document.getElementById("downloadButton").addEventListener("click", incrementDownloadCount);

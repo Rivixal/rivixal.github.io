71dd91fcf1cf4d64e977548d4a6d9c2302854201
Rivixal Software <93471604+Rivixal@users.noreply.github.com>
2023-12-20 02:03:49 +0600

    Delete script directory

--- script/footer.js
+++
@@ -1,1 +0,0 @@
-document.write("<h2>&copy 2020 - ", new Date().getFullYear(),  " Maydilsiel</h2><h3><a href='https://github.com'>Хостится на Github</a></h3><br><p>Автор сайта: Maydilsiel [aka. German Ramzayev]</p><a href='https://github.com/Maydilsiel/maydilsiel.github.io'>Исходный код этого сайта является открытым и доступным каждому.</a><br><p>Работает на версии: May Design 3.0");
--- script/homepage.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.style.opacity = 1;
-  });
-
-
-  

c1e6a58e67eea18d7c73947ce3b47eaf0980e3c7
Rivixal Software <93471604+Rivixal@users.noreply.github.com>
2024-03-09 20:55:56 +0500

    Delete script directory

--- script/footer.js
+++
@@ -1,8 +0,0 @@
-document.write("<div>")
-document.write("<h2>&copy 2020 - ", new Date().getFullYear(),  " Rivixal</h2>");
-document.write("<h2>Автор сайта: German Ramzayev</h2>")
-document.write("<p>Работает на версии: Rivixal UI 1.0</p>")
-document.write("</div>")
-document.write("<br><br>")
-document.write("<h3><a href='https://github.com'>Хостится на Github</a></h3>")
-document.write("<a href='https://github.com/Rivixal/rivixal.github.io'>Исходный код этого сайта является открытым и доступным каждому.</a>")
